37beb1397635c875950dc605d5e7c8c8b8226148
xiaoma <1142322503@qq.com>
2025-12-01 02:24:35 +0800

    苍穹外卖后端登录拦截

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.sky.controller.admin;
 
+
+
+
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -8,7 +11,10 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,35 +36,34 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @Autowired
-    private JwtProperties jwtProperties;
 
+    @Autowired
+    JwtProperties jwtProperties;
     /**
-     * DTO: Data Transfer Object , 数据传输对象
-     * VO : View Object , 返回给前端的数据
-     * Entity: 与数据库表对应的实体类
+     *
+     * DTO:数据传输对象
+     *
+     * VO:View object 服务端返回给前端的对象
+     *
+     * Entity:与数据库表对应
      */
     @PostMapping("/login")
-    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO){
-        log.info("员工登录 , {}" , employeeLoginDTO);
-        Employee employee =  employeeService.login(employeeLoginDTO);
-
-        //生成JWT令牌
+    public Result<EmployeeLoginVO> login(@RequestBody  EmployeeLoginDTO employeeLoginDTO){
+        log.info("员工登录：{}",employeeLoginDTO);
+        Employee employee = employeeService.login(employeeLoginDTO);
+        //封装结果并返回，生成JWT令牌
         Map<String, Object> claims = new HashMap<>();
         claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
-        //claims.put("username", employee.getUsername());
-        //claims.put("name", employee.getName());
         String jwt = JwtUtil.createJWT(jwtProperties.getAdminSecretKey(), jwtProperties.getAdminTtl(), claims);
 
-        //封装结果并返回
-        //EmployeeLoginVO employeeLoginVO = new EmployeeLoginVO(employee.getId(), employee.getUsername(), employee.getName(), jwt);
 
+        //封装数据并返回
+//        EmployeeLoginVO employeeLoginVO = new EmployeeLoginVO(employee.getId(), employee.getUsername(), employee.getName(), jwt);
         EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
                 .id(employee.getId())
                 .name(employee.getName())
                 .userName(employee.getUsername())
-                .token(jwt)
-                .build();
+                .token(jwt).build();
         return Result.success(employeeLoginVO);
     }
 
--- sky-take-out/sky-server/src/main/java/com/sky/interceptor/AdminLoginTokenInterceptor.java
+++ sky-take-out/sky-server/src/main/java/com/sky/interceptor/AdminLoginTokenInterceptor.java
@@ -1,12 +1,14 @@
 package com.sky.interceptor;
 
+
+import com.aliyuncs.utils.StringUtils;
 import com.sky.properties.JwtProperties;
 import com.sky.utils.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
+import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,36 +19,32 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Slf4j
 @Component
-public class AdminLoginTokenInterceptor implements HandlerInterceptor {
+public class AdminLoginTokenInterceptor implements HandlerInterceptor{
 
     @Autowired
     private JwtProperties jwtProperties;
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("拦截到了请求 , {}" , request.getRequestURL().toString());
+        log.info("拦截到了请求，{}",request.getRequestURI().toString());
 
-        //1. 获取请求头中的令牌
+        //1. 获取令牌
         String jwt = request.getHeader(jwtProperties.getAdminTokenName());
-
-        //2. 判断令牌是否存在, 如果不存在, 则不放行 - 401
-        if(!StringUtils.hasLength(jwt)){
-            log.info("令牌为空, 响应401");
+        //2. 判断令牌是否存在。如果不存在，则不放行 - 401
+        if (StringUtils.isEmpty(jwt)){
+            log.info("令牌为空，直接响应401");
             response.setStatus(HttpStatus.SC_UNAUTHORIZED);
             return false;
         }
-
-        //3. 校验令牌 , 如果令牌校验失败 , 则不放行 - 401
+        //3. 校验令牌，如果令牌校验失败， 则不放行 - 401
         try {
             JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), jwt);
         } catch (Exception e) {
             e.printStackTrace();
-            log.info("令牌非法, 响应401");
+            log.info("令牌非法，响应401");
             response.setStatus(HttpStatus.SC_UNAUTHORIZED);
             return false;
         }
-
-        //4. 放行
+        //4.放行
         return true;
     }
 }

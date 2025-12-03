161798065a26f4c7a89c300484821be1b2f9b810
xiaoma <1142322503@qq.com>
2025-12-04 00:00:41 +0800

    扩展spring框架的消息转换器，在转化json格式的数据时，要使用自定义的转换器

--- sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfig.java
+++ sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfig.java
@@ -1,14 +1,22 @@
 package com.sky.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sky.interceptor.AdminLoginTokenInterceptor;
+import com.sky.json.JacksonObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * 配置类
  */
+@Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -18,5 +26,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminLoginTokenInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/employee/login");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展spring框架的消息转换器，在转化json格式的数据时，要使用自定义的转换器");
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+
+        converters.add(0, mappingJackson2HttpMessageConverter);
     }
 }
